fb999c1e7d2b15390cba88a11785ce2e023d7cb7
lou <tolbizeannelou@gmail.com>
2025-06-15 22:06:21 +0400

    Linea first commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+  const projectItems = document.querySelectorAll('.project-item');
+
+  projectItems.forEach(item => {
+    item.addEventListener('mouseenter', () => {
+      projectItems.forEach(i => i.classList.remove('expanded'));
+      item.classList.add('expanded');
+    });
+  });
+
+  document.querySelector('.project-column-gallery').addEventListener('mouseleave', () => {
+    projectItems.forEach(i => i.classList.remove('expanded'));
+    projectItems[0].classList.add('expanded');
+  });
